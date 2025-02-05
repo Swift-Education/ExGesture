9e894d5f967b26ebe5c508eeff6b4206621a3955
kangddong <rkdehddud96@naver.com>
2025-02-05 12:40:00 +0900

    [ADD] DrawLine VC

--- ExGesture/Presentation/RouterViewController.swift
+++ ExGesture/Presentation/RouterViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 final class RouterViewController: UIViewController {
     private let tableView = UITableView(frame: .zero, style: .insetGrouped)
-    private let viewControllers: [String] = ["Action", "AdditiveColor"]
+    private let viewControllers: [RouterPath] = RouterPath.allCases
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,19 +37,43 @@ extension RouterViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         var configuration = cell.defaultContentConfiguration()
-        configuration.text = viewControllers[indexPath.row]
+        configuration.text = viewControllers[indexPath.row].title
         configuration.textProperties.color = .label
         cell.contentConfiguration = configuration
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if indexPath.row == 0 {
+        let router = RouterPath(rawValue: indexPath.row)!
+        switch router {
+        case .gestures:
             let actionVC = ActionViewController()
             navigationController?.pushViewController(actionVC, animated: true)
-        } else {
+        case .additiveColor:
             let additiveColorVC = AdditiveColorViewController()
             navigationController?.pushViewController(additiveColorVC, animated: true)
+        case .drawLine:
+            let drawLineVC = DrawLineViewController()
+            navigationController?.pushViewController(drawLineVC, animated: true)
+        }
+    }
+}
+
+extension RouterViewController {
+    enum RouterPath: Int, CaseIterable {
+        case gestures
+        case additiveColor
+        case drawLine
+        
+        var title: String {
+            switch self {
+            case .gestures:
+                "Do Any Gestures"
+            case .additiveColor:
+                "additiveColor"
+            case .drawLine:
+                "Implementing coalesced touch support in an app"
+            }
         }
     }
 }
